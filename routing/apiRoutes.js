73696107a3da096ab2a36dc1874d44ab0493a241
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-10 15:06:11 -0800

    some working some not

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,52 +1,52 @@
 var friends = require('../data/friends');
 
-module.exports = function(router) {
+module.exports = function(app) {
     // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
-    // A POST routes /api/friends. This will be used to handle incoming survey results. 
-    app.post("/api/friends", function(req, res) {
-        var newFriend = req.body;
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "");
-        console.log('newFriend: ' + newFriend);
+    // A POST routes /api/new. This will be used to handle incoming survey results. 
+    app.post("/api/new", function(req, res) {
+        // var newFriend = req.body;
+        // newFriend.routeName = newFriend.name.replace(/\s+/g, "");
+        // console.log('newFriend: ' + newFriend);
 
-        //This route will also be used to handle the compatibility logic.
-        var friendsCompare = [];
-        // for each item in the "friends" array,{
-        for (var i = 0; i < friends.length; i++) {
-            var sum = 0;
-            // 	for each item in the friends[i].score array {
-            for (var j = 0; j < friends[i].scores.length; j++) {
-                // 	Subtract that score from corresponding score in the newFriend.score array.
-                // 	Return the absolute value of that difference
-                // 	Add the absolute values of all of those differences together together to get the total variance for this item in the friends array.
-                //  Math.abs() returns the absolute value
-                sum = sum + Math.abs(parseInt(newFriend.scores[j]) - parseInt(friends[i].scores[j]));
-            };
-            // push the total variance for this item of the friends array into a new "friendsCopmare" array so that we can capture the variance for each friend.
-            friendsCompare.push(sum);
-        };
+        // //This route will also be used to handle the compatibility logic.
+        // var friendsCompare = [];
+        // // for each item in the "friends" array,{
+        // for (var i = 0; i < friends.length; i++) {
+        //     var sum = 0;
+        //     // 	for each item in the friends[i].score array {
+        //     for (var j = 0; j < friends[i].scores.length; j++) {
+        //         // 	Subtract that score from corresponding score in the newFriend.score array.
+        //         // 	Return the absolute value of that difference
+        //         // 	Add the absolute values of all of those differences together together to get the total variance for this item in the friends array.
+        //         //  Math.abs() returns the absolute value
+        //         sum = sum + Math.abs(parseInt(newFriend.scores[j]) - parseInt(friends[i].scores[j]));
+        //     };
+        //     // push the total variance for this item of the friends array into a new "friendsCopmare" array so that we can capture the variance for each friend.
+        //     friendsCompare.push(sum);
+        // };
 
-        // Get the index of the lowest number in the "friendsCompare" array.  (What if there is a tie?)
-        var index = 0;
-        for (var k = 1; k < friendsCompare.length; k++) {
-            if (friendsCompare[k] < value) {
-                value = friendsCompare[k];
-                index = k;
-            }
-        }
-        // That index is the index from the "friends" array of the newBestie.
-        var newBestie = friends[index]
-            // Display the newBestie as a modal. display both the name and picture of the closest match.
-        $('#newBestieName').html(newBestie.name);
-        $('#newBestiePic').html('<img src="' + newBestie.photo + '" height="42" width="42">');
+        // // Get the index of the lowest number in the "friendsCompare" array.  (What if there is a tie?)
+        // var index = 0;
+        // for (var k = 1; k < friendsCompare.length; k++) {
+        //     if (friendsCompare[k] < value) {
+        //         value = friendsCompare[k];
+        //         index = k;
+        //     }
+        // }
+        // // That index is the index from the "friends" array of the newBestie.
+        // var newBestie = friends[index]
+        //     // Display the newBestie as a modal. display both the name and picture of the closest match.
+        // $('#newBestieName').html(newBestie.name);
+        // $('#newBestiePic').html('<img src="' + newBestie.photo + '" height="42" width="42">');
 
 
-        //push the newFriend into the friends array for future users to find.
-        friends.push(newFriend);
-        //display the newFriend as a JSON in the console.
-        res.json(newFriend);
+        // //push the newFriend into the friends array for future users to find.
+        // friends.push(newFriend);
+        // //display the newFriend as a JSON in the console.
+        // res.json(newFriend);
 
     });
 
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
+var path = require('path');
 //a GET roune to /survey which should display the survey page.
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+module.exports = function(app) {
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
-//A default, catch-all route that leads to home.html which displays the home page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
+    //A default, catch-all route that leads to home.html which displays the home page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+}
