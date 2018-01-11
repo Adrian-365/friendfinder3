8342e7efdaea3db89c10e5fc433b0daae838fb37
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-10 16:57:12 -0800

    Cannot read property '0' of undefied (at apiRoutes.js line 33)

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,42 +11,46 @@ module.exports = function(app) {
         friends.push(req.body);
         //display the newFriend as a JSON in the console.
         res.json(req.body);
+
+        //----------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------
         // var newFriend = req.body;
         // newFriend.routeName = newFriend.name.replace(/\s+/g, "");
         // console.log('newFriend: ' + newFriend);
 
-        // //This route will also be used to handle the compatibility logic.
-        // var friendsCompare = [];
-        // // for each item in the "friends" array,{
-        // for (var i = 0; i < friends.length; i++) {
-        //     var sum = 0;
-        //     // 	for each item in the friends[i].score array {
-        //     for (var j = 0; j < friends[i].scores.length; j++) {
-        //         // 	Subtract that score from corresponding score in the newFriend.score array.
-        //         // 	Return the absolute value of that difference
-        //         // 	Add the absolute values of all of those differences together together to get the total variance for this item in the friends array.
-        //         //  Math.abs() returns the absolute value
-        //         sum = sum + Math.abs(parseInt(newFriend.scores[j]) - parseInt(friends[i].scores[j]));
-        //     };
-        //     // push the total variance for this item of the friends array into a new "friendsCopmare" array so that we can capture the variance for each friend.
-        //     friendsCompare.push(sum);
-        // };
+        //This route will also be used to handle the compatibility logic.
+        var friendsCompare = [];
+        // for each item in the "friends" array,{
+        for (var i = 0; i < friends.length; i++) {
+            var sum = 0;
+            // 	for each item in the friends[i].score array {
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                // 	Subtract that score from corresponding score in the newFriend.score array.
+                // 	Return the absolute value of that difference
+                // 	Add the absolute values of all of those differences together together to get the total variance for this item in the friends array.
+                //  Math.abs() returns the absolute value
+                sum = sum + Math.abs(parseInt(req.body.scores[j]) - parseInt(friends[i].scores[j]));
+            };
+            // push the total variance for this item of the friends array into a new "friendsCopmare" array so that we can capture the variance for each friend.
+            friendsCompare.push(sum);
+        };
 
-        // // Get the index of the lowest number in the "friendsCompare" array.  (What if there is a tie?)
-        // var index = 0;
-        // for (var k = 1; k < friendsCompare.length; k++) {
-        //     if (friendsCompare[k] < value) {
-        //         value = friendsCompare[k];
-        //         index = k;
-        //     }
-        // }
-        // // That index is the index from the "friends" array of the newBestie.
-        // var newBestie = friends[index]
-        //     // Display the newBestie as a modal. display both the name and picture of the closest match.
-        // $('#newBestieName').html(newBestie.name);
-        // $('#newBestiePic').html('<img src="' + newBestie.photo + '" height="42" width="42">');
+        // Get the index of the lowest number in the "friendsCompare" array.  (What if there is a tie?)
+        var index = 0;
+        for (var k = 1; k < friendsCompare.length; k++) {
+            if (friendsCompare[k] < value) {
+                value = friendsCompare[k];
+                index = k;
+            }
+        }
+        // That index is the index from the "friends" array of the newBestie.
+        var newBestie = friends[index]
+            // Display the newBestie as a modal. display both the name and picture of the closest match.
+        $('#newBestieName').html(newBestie.name);
+        $('#newBestiePic').html('<img src="' + newBestie.photo + '" height="42" width="42">');
 
-
+        //-----------------------------------------------------------
+        //-------------------------------------------------------------
 
 
     });
