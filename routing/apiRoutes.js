633c6df6a28cd067f92569aee891e0cf0e9ec9b8
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-11 10:11:50 -0800

    we can now retunr aa best friend

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     // A POST routes /api/new. This will be used to handle incoming survey results. 
     app.post("/api/new", function(req, res) {
         //push the newFriend into the friends array for future users to find.
-        friends.push(req.body);
+        // friends.push(req.body);
         //display the newFriend as a JSON in the console.
         res.json(req.body);
 
@@ -17,9 +17,9 @@ module.exports = function(app) {
         //-------------------------------------------------------------------------------
         var newAdd = (req.body);
         // newAdd.routeName = newAdd.name.replace(/\s+/g, "");
-        console.log(newAdd);
-        console.log(req.body);
-
+        // console.log(newAdd);
+        // console.log(req.body);
+        console.log(req.body['scores[]']);
         //This route will also be used to handle the compatibility logic.
         var friendsCompare = [];
         // for each item in the "friends" array,{
@@ -33,18 +33,17 @@ module.exports = function(app) {
                 // 	Return the absolute value of that difference
                 // 	Add the absolute values of all of those differences together together to get the total variance for this item in the friends array.
                 //  Math.abs() returns the absolute value
-                // var b = friends[i].scores[j];
-                //*** Cannot read property '0' of undefined (below var a... I newAdd.scores may not be coming thorugh as an array?):
-                // var a = newAdd.scores[j];
 
-                sum = sum + Math.abs(parseInt(newAdd['scores[]'][j]) - parseInt(existFriendsScores[j]));
+                sum = sum + Math.abs(parseInt(req.body['scores[]'][j]) - parseInt(existFriendsScores[j]));
             };
             // push the total variance for this item of the friends array into a new "friendsCopmare" array so that we can capture the variance for each friend.
             friendsCompare.push(sum);
         };
+        console.log(friendsCompare);
 
         // Get the index of the lowest number in the "friendsCompare" array.  (What if there is a tie?)
         var index = 0;
+        var value = friendsCompare[0];
         for (var k = 1; k < friendsCompare.length; k++) {
             if (friendsCompare[k] < value) {
                 value = friendsCompare[k];
@@ -52,8 +51,9 @@ module.exports = function(app) {
             }
         }
         // That index is the index from the "friends" array of the newBestie.
-        var newBestie = friends[index]
-            // Display the newBestie as a modal. display both the name and picture of the closest match.
+        var newBestie = friends[index];
+        console.log(newBestie);
+        // Display the newBestie as a modal. display both the name and picture of the closest match.
         $('#newBestieName').html(newBestie.name);
         $('#newBestiePic').html('<img src="' + newBestie.photo + '" height="42" width="42">');
 
