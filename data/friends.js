06d0910bc3c549581e096ead22105858629822d5
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-11 17:50:01 -0800

    works.  needs validation tho

--- data/friends.js
+++ data/friends.js
@@ -1,51 +1,51 @@
 var friends = [{
         name: "Daisy",
-        photo: "https://goo.gl/images/QAiKn9",
+        photo: "http://www.slate.com/content/dam/slate/articles/life/doonan/2014/09/140917_DOONAN_DogPainting.jpg.CROP.thumbnail-small.jpg",
         scores: [5, 1, 4, 4, 1, 3, 2, 5, 1, 4]
     },
     {
         name: "Rocky",
-        photo: "https://goo.gl/images/rvbCc5",
+        photo: "https://vdra3q3o9x-flywheel.netdna-ssl.com/wp-content/uploads/2015/04/Barking-dog-thumbnail-edit-resize.jpg",
         scores: [4, 2, 5, 2, 4, 4, 2, 3, 2, 5]
     },
     {
         name: "Ralphie",
-        photo: "https://goo.gl/images/vyg9Ax",
+        photo: "https://lh6.ggpht.com/EoH-UNwa_XRNuk0jB7UQDVQdTSigPu4B5zrb3I5iXk289KYfGZOzJgfwP8Y9DEA1O_k=h310",
         scores: [3, 1, 4, 1, 3, 5, 2, 4, 3, 3]
     },
     {
         name: "Suzanna",
-        photo: "https://goo.gl/images/UbNbTw",
+        photo: "http://www.petmania.ie/images/default-source/grooming-price-lists/total-wellbeing-package-thumbnail.jpg?sfvrsn=2",
         scores: [2, 4, 2, 5, 2, 1, 2, 3, 2, 5]
     },
     {
         name: "Star",
-        photo: "https://goo.gl/images/9Uu5dF",
+        photo: "https://s7rx998xqs-flywheel.netdna-ssl.com/wp-content/uploads/2014/07/dog-on-grass-thumbnail-edit1.jpg",
         scores: [1, 4, 3, 3, 5, 2, 2, 1, 1, 4]
     },
     {
         name: "Pepper",
-        photo: "https://goo.gl/images/RzPyc2",
+        photo: "https://i.pinimg.com/736x/bf/6e/d3/bf6ed3d86e0b7bb9225926775a0d8e95--british-accent-british-shorthair-kittens.jpg",
         scores: [5, 1, 4, 3, 1, 3, 2, 3, 4, 1]
     },
     {
         name: "Wheelie",
-        photo: "https://goo.gl/images/QKZeEB",
+        photo: "http://1389blog.com/pix/happy-cat-wallpaper-thumbnail.jpg",
         scores: [4, 3, 5, 2, 1, 4, 2, 4, 3, 2]
     },
     {
         name: "Peaches",
-        photo: "https://goo.gl/images/RU53hy",
+        photo: "http://www.slate.com/content/dam/slate/articles/health_and_science/science/2013/01/130123_SCI_EvilCats.jpg.CROP.thumbnail-small.jpg",
         scores: [3, 1, 1, 2, 4, 5, 2, 5, 2, 3]
     },
     {
         name: "The Dread Pirate Roberts",
-        photo: "https://goo.gl/images/Z7DGhd",
+        photo: "https://lh3.googleusercontent.com/PWL_r_v2760JdZ_w9d5r97yGchDvYisMoeYsJ-1-wzA0hYspRa6sZSwbl6i6JTSl0w=h310",
         scores: [3, 2, 5, 2, 1, 1, 2, 1, 3, 1]
     },
     {
         name: "Rocket",
-        photo: "https://goo.gl/images/6Lbf3C",
+        photo: "http://i0.kym-cdn.com/entries/icons/original/000/007/263/photo_cat2.jpg",
         scores: [1, 5, 3, 4, 5, 2, 2, 5, 1, 3]
     }
 ]
